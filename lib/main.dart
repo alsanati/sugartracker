4e14a9dbe763fceb249e113969767c52d391f7c0
Atilla Alsan <alsan.atilla@icloud.com>
2023-02-22 21:00:56 +0100

    fixed typo

--- lib/main.dart
+++ lib/main.dart
@@ -97,9 +97,8 @@ final router = GoRouter(
 
 Future<void> main() async {
   await Supabase.initialize(
-    url: "https://vplernwrerwdcyoymbwx.supabase.co",
-    anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwbGVybndyZXJ3ZGN5b3ltYnd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc5MTUyMTUsImV4cCI6MTk4MzQ5MTIxNX0.KW-RIscnzX4UKFCQ7oMVDckZIJwyOkF2fUmLH-QM6tc",
+    url: Constants.supabaseUrl,
+    anonKey: Constants.supabaseAnonKey,
   );
   runApp(ProviderScope(child: MyApp()));
 }
